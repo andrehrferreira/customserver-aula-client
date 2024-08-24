ecd4ae1702befc61d0d3efdc5e3afe0d61e0cc01
Andre <andre@tos.world>
2024-08-24 10:10:57 -0300

    bytebuffer and queuebuffer

--- Source/MMORPGDoZero/MMORPGDoZero.Build.cs
+++ Source/MMORPGDoZero/MMORPGDoZero.Build.cs
@@ -10,18 +10,14 @@ public class MMORPGDoZero : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "WebSockets" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "Engine",
-                "Slate",
-                "SlateCore" });
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-        PublicDefinitions.Add("WITH_WEBSOCKETS=1");
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-        // Uncomment if you are using Slate UI
-        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-        // Uncomment if you are using online features
-        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-    }
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
 }
--- Source/MMORPGDoZero/Public/Websocket.h
+++ Source/MMORPGDoZero/Public/Websocket.h
@@ -1,9 +1,9 @@
-// Copyright 2022 Chris Ringenberg https://www.ringenberg.dev/
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "UObject/Object.h"
+#include "ByteBuffer.h"
+#include "Modules/ModuleManager.h"
 
 #include "Websocket.generated.h"
 
@@ -13,6 +13,7 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnWebSocketConnected);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnWebSocketConnectionError, const FString&, Error);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnWebSocketClosed, int32, StatusCode, const FString&, Reason, bool, bWasClean);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnWebSocketMessageReceived, const FString&, Message);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnWebSocketBinaryMessageReceived, UByteBuffer*, Data);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnWebSocketMessageSent, const FString&, Message);
 
 UCLASS(MinimalAPI, BlueprintType)
@@ -21,7 +22,6 @@ class UWebSocket final : public UObject
 	GENERATED_BODY()
 
 public:
-
 	UPROPERTY(BlueprintAssignable)
 	FOnWebSocketConnected OnWebSocketConnected;
 
@@ -35,21 +35,27 @@ public:
 	FOnWebSocketMessageReceived OnWebSocketMessageReceived;
 
 	UPROPERTY(BlueprintAssignable)
+	FOnWebSocketBinaryMessageReceived OnWebSocketBinaryMessageReceived;
+
+	UPROPERTY(BlueprintAssignable)
 	FOnWebSocketMessageSent OnWebSocketMessageSent;
 
 	void InitWebSocket(TSharedPtr<IWebSocket> InWebSocket);
 
-	UFUNCTION(BlueprintCallable, Category = "Easy WebSockets")
+	UFUNCTION(BlueprintCallable, Category = "WebSockets")
 	void Connect();
 
-	UFUNCTION(BlueprintCallable, Category = "Easy WebSockets")
+	UFUNCTION(BlueprintCallable, Category = "WebSockets")
 	void Close(int32 StatusCode = 1000, const FString& Reason = TEXT(""));
 
-	UFUNCTION(BlueprintPure, Category = "Easy WebSockets")
+	UFUNCTION(BlueprintPure, Category = "WebSockets")
 	bool IsConnected() const;
 
-	UFUNCTION(BlueprintCallable, Category = "Easy WebSockets")
-	void SendMessage(const FString& Message);
+	UFUNCTION(BlueprintCallable, Category = "WebSockets")
+	void SendMessage(uint8 PacketType, UByteBuffer* Message);
+
+	UFUNCTION(BlueprintCallable, Category = "WebSockets")
+	void SendEncryptedMessage(uint8 PacketType, UByteBuffer* Message, const FString& Key);
 
 private:
 
@@ -65,9 +71,28 @@ private:
 	UFUNCTION()
 	void OnWebSocketMessageReceived_Internal(const FString& Message);
 
+	void OnWebSocketBinaryMessageReceived_Internal(const void* Data, SIZE_T Size, bool bIsBinary);
+
 	UFUNCTION()
 	void OnWebSocketMessageSent_Internal(const FString& Message);
 
 	TSharedPtr<IWebSocket> InternalWebSocket;
+};
 
+
+UCLASS(MinimalAPI)
+class UWebSocketFunctionLibrary final : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "WebSockets")
+	static UWebSocket* CreateWebSocket(FString ServerUrl, FString ServerProtocol = TEXT("ws"));
+
+	UFUNCTION(BlueprintCallable, Category = "WebSockets")
+	static UWebSocket* CreateWebSocketWithHeaders(FString ServerUrl, TMap<FString, FString> UpgradeHeaders, FString ServerProtocol = TEXT("ws"));
+
+	UFUNCTION(BlueprintCallable, Category = "WebSockets")
+	static int32 GetTimeInMilliseconds();
 };
